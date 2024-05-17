d9725549c3d73b39ae45424223ccf2526953667d
Felipe Santana <felipe012santana012@gmail.com>
2024-05-17 17:35:04 -0300

    finalizado implementação de carrinho para pedidos

--- src/js/scriptComidas.js
+++ src/js/scriptComidas.js
@@ -1,4 +1,7 @@
 
+
+
+
 let sectionCategoriasCards = document.querySelector('.todas__comidas__pratos__categorias')
 cardapio.forEach(itemCategoria => {
 
@@ -31,7 +34,7 @@ cardapio.forEach(itemCategoria => {
                     </ul>
                     <p class="comida__descricoes-final">${item.descricao}</p>
                     <div class="comida__descricoes-btn-icon">
-                        <button class="comida__descricoes-btn" data-pegar-elemento >Pedir</button>
+                        <button class="comida__descricoes-btn" data-pegar-elemento >Adicionar</button>
                         <div class="comida__descricoes-contador">
                             <i class="fa-solid fa-plus" data-aumentar-quantidade></i>
                             <span data-quantidade-produto>0</span>
@@ -57,6 +60,7 @@ cardapio.forEach(itemCategoria => {
     let btnItem = document.querySelectorAll('.comida__descricoes-btn')
     let quantidadeProduto = document.querySelectorAll("[data-quantidade-produto]");
     let menssagemError = document.querySelectorAll(".menssagem__error");
+   let carrinho =  document.querySelector('.carrinho-pedidos')
 
     btnItem.forEach((item, index) => {
         item.setAttribute('data-pegar-elemento', index);
@@ -65,9 +69,11 @@ cardapio.forEach(itemCategoria => {
             quantidadeProduto[index].getAttribute("data-quantidade-produto");
             let quantidade = parseInt(quantidadeProduto[index].textContent)
             if(quantidade>0){
-                
+                carrinho.style.display = 'block'
+                carrinhoIcone.style.display = "none"
                 menssagemError[index].style.display = "none"
-                addValorMenssagem(todosItens[index],quantidade)           
+                addValorMenssagem(todosItens[index],quantidade)  
+
             }else{  
                 menssagemError[index].textContent = "Selecione no mínimo 1 item."; 
                 menssagemError[index].style.display = "inline"
@@ -77,10 +83,14 @@ cardapio.forEach(itemCategoria => {
         })
     })
 
-
-    let todosPedidos = []
-
+    if (!localStorage.getItem('Pedidos')) {
+        // Se não existir, inicializa a chave com um array vazio
+        localStorage.setItem('Pedidos', JSON.stringify([]));
+    }
+    
+    let todosPedidos = JSON.parse(localStorage.getItem('Pedidos'))
     function addValorMenssagem(item, quantidade){
+        
         let valorFinalItem = item.precoOriginal * quantidade
         let menssagem = ` ${quantidade} x ${item.nome} no valor de ${item.precoOriginal}. Total a Pagar: ${valorFinalItem.toFixed(2)}. `
 
@@ -94,16 +104,72 @@ cardapio.forEach(itemCategoria => {
         todosPedidos.push(pedido)
         console.log(todosPedidos)
         localStorage.setItem('Pedidos', JSON.stringify(todosPedidos))
+        getPedidoLocalStorage()
         // const urlWhatsApp = `https://wa.me/5581988742454?text=${encodeURIComponent(menssagem)}`;
-        // window.open(urlWhatsApp);
+        // window.open(urlWhatsApp);   
+    }
+    getPedidoLocalStorage()
+   
+
+    // let criarPedidoLocalStorage = (pedido) =>{
+    //     let itensPedido =  JSON.parse(localStorage.getItem('Pedidos',itensPedido))
+    //     itensPedido.push(pedido)
+    //     localStorage.setItem('Pedidos', JSON.stringify(itensPedido))
+    // }
+
+    
+    function getPedidoLocalStorage(){
         
+       let itensPedidoConvertido = JSON.parse(localStorage.getItem('Pedidos',todosPedidos)) ?? []
+
+        
+
+       let carrinhoListaItens = document.querySelector('.carrinho__lista__itens')
+       let totalPagar = document.querySelector('.carrinho__valor-total span')
+       let somaTotalPagar =0
+
+       carrinhoListaItens.textContent = ''
+
+       itensPedidoConvertido.forEach((item,index) =>{
+        let li = document.createElement('li')
+            li.classList.add('carrinho__item')
+            li.innerHTML += `
+            <p class="carrinho__item-quantidade">${item.quantidade}<span>x</span></p>
+            <h3 class="carrinho__item-titulo">${item.nome}</h3>                   
+            <p class="carrinho__item-valor">${item.precoOriginal}<span>R$</span></p>
+            <i class="fa-regular fa-trash-can" id="deletar-item"></i>
+            
+            `
+            carrinhoListaItens.append(li)
+            let x =  JSON.parse(item.precoFinal)
+            somaTotalPagar += x
+                 
+
+       })
+       
+       
+       totalPagar.textContent = somaTotalPagar.toFixed(2)
     }
 
+    console.log('oiiiiiiiiiii')
+
    
-    
 
-       
 
+    let carrinhoIcone = document.querySelector('.carrinho-icone')
+    let carrinhoFechar = document.querySelector('.carrinho__fechar')
+
+    carrinhoIcone.addEventListener('click', () =>{
+        carrinhoIcone.style.display = "none"
+        carrinho.style.display = "block"
+    })
+
+    carrinhoFechar.addEventListener('click', () => {
+        carrinhoIcone.style.display = "block"
+        carrinho.style.display = "none"
+    })
+
+   
         
     
 
